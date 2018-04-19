cca6825fa3022a66b7423f89cf85cc5edf0bf766
allan315 <allanpilvet@gmail.com>
2018-04-20 00:19:03 +0300

    pep8 fix

--- database_setup.py
+++ database_setup.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column,Integer,String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
@@ -6,33 +6,27 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-class User(Base):
-  __tablename__ = 'user'
-  id = Column(Integer, primary_key=True)
-  name = Column(String(250), nullable=False)
-  email = Column(String(250), nullable=False)
 
 class Items(Base):
-  __tablename__ = 'items'
-  id = Column(Integer, primary_key = True)
-  item_name = Column(String)
-  item_link = Column(String)
-  item_description = Column(String)
-  item_image = Column(String)
-  
-  
-  #Add a property decorator to serialize information from this database
-  @property
-  def serialize(self):
-    return {
-      'item_name': self.item_name,
-      'item_link': self.item_link,
-      'item_image' : self.item_image,
-      'item_description' : self.item_description,
-      'id' : self.id
-      }
+    __tablename__ = 'items'
+    id = Column(Integer, primary_key=True)
+    item_name = Column(String)
+    item_link = Column(String)
+    item_description = Column(String)
+    item_image = Column(String)
+
+    #  Add a property decorator to serialize information from this database
+
+    @property
+    def serialize(self):
+        return {
+            'item_name': self.item_name,
+            'item_link': self.item_link,
+            'item_image': self.item_image,
+            'item_description': self.item_description,
+            'id': self.id
+            }
 
 engine = create_engine('sqlite:///items.db')
- 
-
 Base.metadata.create_all(engine)
+
